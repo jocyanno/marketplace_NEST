bf985d02891fd9f6ed48f8f5ba0e277462b88c83
jocyannovittor@hotmail.com <jocyannovittor@hotmail.com>
2026-04-25 20:02:41 -0300

    feat: initial configs and proxy module

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { AppService } from './app.service';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ProxyModule } from './proxy/proxy.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+    ProxyModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
